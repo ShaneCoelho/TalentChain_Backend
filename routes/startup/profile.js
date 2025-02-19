2a04f5cee93c85b35fed8a65eb5fa41f1ad2eded
ShaneCoelho <shanecoelho1@gmail.com>
2025-02-19 19:36:40 +0530

    added role to the startup register route

--- routes/startup/profile.js
+++ routes/startup/profile.js
@@ -8,13 +8,13 @@ const Users = mongoose.model('Users');
 router.post('/startupregister', fetchusers, async (req, res) => {
 
     const userId = req.user.id;
-    const { name, regno, companytype, existingUser } = req.body;
+    const { name, regno, companytype, existingUser, role } = req.body;
 
     try {
         
         const updatedUser = await Users.findByIdAndUpdate(
             userId,
-            { name, regno, companytype, existingUser },
+            { name, regno, companytype, existingUser, role },
             { new: true, runValidators: true } // Option to return the updated document
         );
 
@@ -22,7 +22,11 @@ router.post('/startupregister', fetchusers, async (req, res) => {
             return res.status(404).json({ message: "User not found" });
         }
 
-        res.status(200).json(updatedUser);
+        res.status(200).send({
+            role: role,
+            name: name,
+            image: req.user.image || null, // Send null if image is not provided
+        });
     } catch (error) {
         
         res.status(500).json({ message: "An error occurred", error });
